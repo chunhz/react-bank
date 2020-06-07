636629f7768345e60a1da2c4a9cf92d4b4666d0d
Chunhao Zheng <chz062009@gmail.com>
2020-06-07 18:59:00 -0400

    app deploy

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ class Home extends Component {
           <img src="https://letstalkpayments.com/wp-content/uploads/2016/04/Bank.png" alt="bank"/>
           <h1>Bank of React</h1>
           <strong><AccountBalance accountBalance = {this.props.accountBalance}/> </strong>
-          <div><Link></Link></div>
+          <div><Link to = "/"></Link></div>
           <div><Link to = "/UserProfile">User Profile</Link></div>
           <div><Link to = "/Login">Log In</Link></div>
           <div><Link to = "/Debits">Debits</Link></div>
