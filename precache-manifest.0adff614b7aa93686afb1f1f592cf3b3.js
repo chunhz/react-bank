a99e2c2f70ca961083288ae28beea7a22615e652
Chunhao Zheng <chz062009@gmail.com>
2020-06-07 18:37:52 -0400

    Updates

---
+++ precache-manifest.0adff614b7aa93686afb1f1f592cf3b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "069bde38573f58b0ddc34c34fd126945",
+    "url": "/react-bank/index.html"
+  },
+  {
+    "revision": "e8fde2d5ee1f3430ba7e",
+    "url": "/react-bank/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "41849c1c048f6c7c7ff0",
+    "url": "/react-bank/static/js/2.3fe149e0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-bank/static/js/2.3fe149e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8fde2d5ee1f3430ba7e",
+    "url": "/react-bank/static/js/main.490d8271.chunk.js"
+  },
+  {
+    "revision": "26655f73963a004f46a2",
+    "url": "/react-bank/static/js/runtime-main.632d4a43.js"
+  }
+]);
